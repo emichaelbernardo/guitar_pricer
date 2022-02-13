cba44577ce0dba1bd6309e9306b091ab2150968f
emilbernardo <emichaelbernardo@gmail.com>
2022-02-13 00:48:45 -0500

    updated app file

--- guitar_2.py
+++ guitar_2.py
@@ -1,33 +1,16 @@
-
+import math
 import pickle
 import streamlit as st
  
 # loading the trained model
-pickle_in = open('models/guitar.pkl', 'rb') 
-model = pickle.load(pickle_in)
+# Load the model
+with open('models/guitar_test.pkl', 'rb') as f:
+    model = pickle.load(f)
  
 @st.cache()
   
 # defining the function which will make the prediction using the data which the user inputs 
 def prediction(cond_score, type_code, price_range):   
- 
-    # Pre-processing user input    
-    #if Gender == "Male":
-    #    Gender = 0
-    #else:
-    #    Gender = 1
- 
-    #if Married == "Unmarried":
-    #    Married = 0
-    #else:
-    #    Married = 1
- 
-    #if Credit_History == "Unclear Debts":
-    #    Credit_History = 0
-    #else:
-    #    Credit_History = 1  
- 
-    #LoanAmount = LoanAmount / 1000
  
     # Making predictions 
     prediction = model.predict( 
@@ -38,25 +21,77 @@ def prediction(cond_score, type_code, price_range):
 def main():       
     # front end elements of the web page 
     html_temp = """ 
-    <div style ="background-color:yellow;padding:13px"> 
-    <h1 style ="color:black;text-align:center;">Streamlit Loan Prediction ML App</h1> 
+    <div style ="background-color:lightblue;padding:13px"> 
+    <h1 style ="color:black;text-align:center;">Guitar Price Prediction ML App</h1> 
     </div> 
     """
       
     # display the front end aspect
     st.markdown(html_temp, unsafe_allow_html = True) 
     
+    #def format_func(box,choice_dict):
+    #        return choice_dict[box]
+
+   
+
+    
+    
+    conditions = { 
+        6: "Mint",
+        5: "Excellent",
+        4: "Very Good",
+        3: "Good",
+        2: "Fair",
+        1: "Poor"
+        }   
+
     cond_score = st.selectbox(
-     'condition?',(1, 2, 3, 4, 5, 6))
+        label="Condition of guitar:",
+        options= (6,5, 4, 3, 2, 1), 
+        format_func=lambda x: conditions.get(x),
+        )   
+    
+    types = { 
+        1: "Solid Body",
+        2: "Semi-Hollow",
+        3: "Hollow Body",
+        4: "Other"
+        
+        }   
 
     type_code = st.selectbox(
-     'type?',(1, 2, 3, 4, 5, 6))
+        label="Body type of guitar:",
+        options= (1,2,3,4), 
+        format_func=lambda x: types.get(x),
+        )  
+   
 
+
+    price_dict = {1: "Not Sure.",
+                  2: "less than 500",
+                  3: "less than 1000",
+                  4: "less than 1500",
+                  5: "less than 2500" }
+    
     price_range = st.selectbox(
-     'How much did you pay for it?',(1, 2, 3, 4, 5, 6))
+        label="How much was it originally:",
+        options= (5, 4, 3, 2, 1), 
+        format_func=lambda x: price_dict.get(x),
+        )       
+    
+    #price_range = st.selectbox(
+    #'How much did you pay for it?',(1, 2, 3, 4, 5, 6))
+    #,  
+    #options=list(price_dict.keys(), format_func=format_func(price_range,price_dict))
     
     # when 'Predict' is clicked, make the prediction and store it 
     if st.button("Predict"): 
-        result = prediction(cond_score, type_code, price_range) 
-        st.success('value is  {}'.format(result))
+        result = prediction(cond_score, type_code, price_range)
+        price = "{:.2f}".format(result[0])
         
+        #st.write('Guitar value is $',price)
+        st.success('Guitar value is ${}'.format(price))
+        
+        
+if __name__=='__main__': 
+    main()        
