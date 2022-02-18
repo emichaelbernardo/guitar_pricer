f3d312a64b8ae9f23278dfc402bdcc5739fe1582
emilbernardo <emichaelbernardo@gmail.com>
2022-02-18 18:43:28 -0500

    updated py

--- guitar_2.py
+++ guitar_2.py
@@ -2,12 +2,36 @@ import math
 import pickle
 import sklearn
 import streamlit as st
+import boto3
+import boto3.session
+from PIL import Image
+import configparser
  
-# loading the trained model
-# Load the model
-with open('models/guitar_test.pkl', 'rb') as f:
-    model = pickle.load(f)
- 
+# loading the trained model from local drive
+#with open('models/guitar_test.pkl', 'rb') as f:
+#    model = pickle.load(f)
+    
+
+## loading the trained model from AWS S3 
+
+config = configparser.ConfigParser()
+config.read('aws.ini')
+    
+AWS_key_id     = config['aws']['aws_access_key_id']
+AWS_secret_key = config['aws']['aws_secret_access_key']   
+    
+
+# Creating the low level functional client
+client = boto3.client(
+    's3',
+    aws_access_key_id = AWS_key_id,
+    aws_secret_access_key =  AWS_secret_key,
+    region_name = 'us-east-1'
+)
+response = client.get_object(Bucket='dataforguitarapp', Key='guitar_test.pkl')
+body = response['Body'].read()
+model = pickle.loads(body)    
+
 @st.cache()
   
 # defining the function which will make the prediction using the data which the user inputs 
@@ -22,13 +46,33 @@ def prediction(cond_score, type_code, price_range):
 def main():       
     # front end elements of the web page 
     html_temp = """ 
-    <div style ="background-color:lightblue;padding:13px"> 
+    <div style ="background-image: url("bg.png");
+    background-size: cover;"> 
+    
     <h1 style ="color:black;text-align:center;">Guitar Price Prediction ML App</h1> 
     </div> 
     """
-      
+    # def img_to_bytes(img_path):
+    #img_bytes = Path(bg.png).read_bytes()
+    #encoded = base64.b64encode(img_bytes).decode()
+    #return encoded
+    img = Image.open("bg.png")
+    st.image(img)
+
+    
+    
+    
     # display the front end aspect
-    st.markdown(html_temp, unsafe_allow_html = True) 
+    #st.markdown(html_temp, unsafe_allow_html = True) 
+    
+    #header_html = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(encoded)
+     
+    
+    #header_html = "<img src='bg.png'>"
+    
+    #st.markdown(
+    #    header_html, unsafe_allow_html=True,
+    #)    
     
     #def format_func(box,choice_dict):
     #        return choice_dict[box]
@@ -47,7 +91,7 @@ def main():
         }   
 
     cond_score = st.selectbox(
-        label="Condition of guitar:",
+        label="the Condition of guitar:",
         options= (6,5, 4, 3, 2, 1), 
         format_func=lambda x: conditions.get(x),
         )   
