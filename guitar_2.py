31ece773f085c9a413b19bee509e3268242462f6
emilbernardo <emichaelbernardo@gmail.com>
2022-02-20 13:16:15 -0500

    updated app

--- guitar_2.py
+++ guitar_2.py
@@ -8,7 +8,7 @@ import boto3
 from botocore.exceptions import ClientError
 import os
 
-from PIL import Image
+#from PIL import Image
 import configparser
 
 #st.write("aws_access:", st.secrets["aws_access"])
